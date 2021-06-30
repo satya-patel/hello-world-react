0d358cf49123db98606aa92dcd903eb80ce60389
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-30 15:51:36 +0530

    uc4: ability to launch bridgelab site in new tab by on click logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/BridgeLabz.jpg'
-class App extends React.Component {
 
+class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super()
     this.state = {
       title: 'hello world from bridgelabz'
     }
   }
+
+  //onClick Function
+  onClick = (Sevent) => {
+    console.log("save button is clicked", Sevent);
+    window.open(this.url, "._blank");
+  }
   render() {
-    return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo}
+  return (
+    <div>
+      <h1>{this.state.title}</h1>
+      <img src={logo} onClick={this.onClick}
         alt="The BridgeLabz logo : a Bridge to Employment through lab works" />
       </div>
     );
