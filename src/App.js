2279e5b9ccc55cb7aef26edcccf8660519fd6488
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-30 16:05:31 +0530

    uc5: display hello form BL with user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      title: 'hello world from bridgelabz'
+      userName: ''
     }
   }
 
@@ -16,13 +16,26 @@ class App extends React.Component {
     console.log("save button is clicked", Sevent);
     window.open(this.url, "._blank");
   }
+  onNameChange = (event) => {
+    console.log("value is",event.target.value);
+    //set the title using setstate method
+    this.state({userName: event.target.value})
+  }
   render() {
   return (
+    <>
     <div>
-      <h1>{this.state.title}</h1>
+      <h1>hello{this.state.userName}from Bridgelabz</h1>
       <img src={logo} onClick={this.onClick}
         alt="The BridgeLabz logo : a Bridge to Employment through lab works" />
       </div>
+
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
+    </>
+
+
     );
   }
 }
