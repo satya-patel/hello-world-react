e1054603db8761b4c0e845a4ebe80e95612cb66d
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-30 15:43:35 +0530

    uc3: Display Bl Logo by proprty binding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import logo from './assets/BridgeLabz.jpg'
 class App extends React.Component {
 
   constructor(){
@@ -13,9 +13,10 @@ class App extends React.Component {
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo}
+        alt="The BridgeLabz logo : a Bridge to Employment through lab works" />
       </div>
     );
   }
 }
-
 export default App;
